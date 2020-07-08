a02bb542adb77505a0f54fa552f34c40a749d5c6
Kyle Morgan <kmorgan8588@gmail.com>
2020-07-08 16:09:08 -0700

    staged package.json and updated with another test for isUniqueChars

--- Chapter_1/UniqueChars/isUniqueChar.test.js
+++ Chapter_1/UniqueChars/isUniqueChar.test.js
@@ -5,4 +5,19 @@ test('an empty string should return true', () => {
 	expect(isUniqueChars('')).toBe(true);
 });
 
+test('a string of length one should return true', () => {
+	expect(isUniqueChars('a')).toBe(true);
+	expect(isUniqueChars('e')).toBe(true);
+	expect(isUniqueChars('r')).toBe(true);
+	expect(isUniqueChars('u')).toBe(true);
+	expect(isUniqueChars('x')).toBe(true);
+	expect(isUniqueChars('y')).toBe(true);
+});
 
+test('banana should fail', () => {
+	expect(isUniqueChars('Banana')).toBe(false);
+});
+
+test('upper and lowercase alphabet should pass', () => {
+	expect(isUniqueChars('AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz')).toBe(true);
+});
